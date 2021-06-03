c8d1ac6236171b03cc4cb8184ce25baecf89148b
Artur Moraes <artur.gomoraes@gmail.com>
2021-06-03 14:02:40 -0300

    Initial commit

---
+++ C# I/AprendendoCSharp/Teste/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor = 1000;
+
+            for(int contador = 1; contador <= 12; contador++)
+            {
+                valor *= 1.0036;
+                Console.WriteLine("Após " + contador + " meses, você terá R$" + valor);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
